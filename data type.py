338d3b9e8b9e6a8c4ab6da7f39d13c3c9d8b82f4
MD. AL-AMIN ARIF <arif.ru.eee@gmail.com>
2021-08-18 23:28:35 +0600

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,25 @@
+#integer type
+age=22
+print(type(age))
+
+#Float type
+weight=60.5
+print(type(weight))
+
+#Complex type
+z=3+4j
+print(type(z))
+
+#string type
+Firstname="Al-amin"
+Secondname="Arif"
+print(type(Firstname))
+print(type(Secondname))
+print(Firstname+Secondname)
+print(Firstname.lower())
+
+#Boolean type
+isMale=True
+isFemale=False
+print(type(isMale))
+print(isMale == isFemale)
---
+++ variable.py
@@ -0,0 +1,13 @@
+from typing import Type
+
+
+name='Al-amin'
+print(name)
+num=45
+print(45)
+first_name='arif'
+print(first_name)
+import keyword
+print(keyword.kwlist)
+
+
